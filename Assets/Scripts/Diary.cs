bc799021bdb4e495adaec8dcf7a0a9f207f134c3
NeoLjusica <jessiicaceder@hotmail.com>
2023-04-22 20:32:13 +0200

    Diary logic working

--- Assets/Scripts/Diary.cs
+++ Assets/Scripts/Diary.cs
@@ -6,41 +6,121 @@ using UnityEngine;
 public class Diary : MonoBehaviour
 {
     public GameObject diary;
-    public GameObject flipButton;
     public GameObject[] diaryNotes;
+    public GameObject flipForwardsButton;
+    public GameObject flipBackwardsButton;
 
-    bool active;
+    private GameObject[] enabledDiaryNotes;
+
+    bool[] noteEnabled = { false, false, false, false, false, false, false };
 
     private void Start()
     {
-        active = false;
+        enabledDiaryNotes = new GameObject[7];
     }
 
     public void OpenOrCloseDiary()
     {
-        if (!active)
-        {
-            diary.gameObject.SetActive(true);
-            flipButton.gameObject.SetActive(true);
+        diary.gameObject.SetActive(!diary.gameObject.activeSelf);
+        flipBackwardsButton.gameObject.SetActive(false);
+        flipForwardsButton.gameObject.SetActive(false);
 
-            active = true;
-        }
-        else
+        if (enabledDiaryNotes[0] == diaryNotes[0])
         {
-            diary.gameObject.SetActive(false);
-            flipButton.gameObject.SetActive(false);
-            
-            active = false;
+            enabledDiaryNotes[0].gameObject.SetActive(diary.gameObject.activeSelf);
+
+            if (noteEnabled[1]) { flipForwardsButton.gameObject.SetActive(true); }
+        }
+
+        if(!diary.gameObject.activeSelf)
+        {
+            foreach(GameObject note in enabledDiaryNotes) 
+            { 
+                if(note != null)
+                {
+                    if(note.activeSelf)
+                        note.SetActive(false); 
+                }
+            }
         }
     }
 
-    private void SetNextNoteActive()
+    public void FlipForwards()
     {
-        for (int i = 0; i < diaryNotes.Length; i++)
+        //TODO: Do some flip-animation
+        if (enabledDiaryNotes != null)
         {
-            if (!diaryNotes[i].gameObject.activeSelf)
+            for (int i = 0; i < enabledDiaryNotes.Length; i++)
             {
-                diaryNotes[i].SetActive(true);
+                if (enabledDiaryNotes[i] == null) { continue; }
+                if (enabledDiaryNotes[i].gameObject.activeSelf)
+                {
+                    enabledDiaryNotes[i].gameObject.SetActive(false);
+
+                    if ((i + 1) <= enabledDiaryNotes.Length && noteEnabled[i + 1] != false)
+                    {
+                        enabledDiaryNotes[i + 1].gameObject.SetActive(true);
+                        flipBackwardsButton.gameObject.SetActive(true);
+
+                        if ((i + 2) >= enabledDiaryNotes.Length || noteEnabled[i + 2] != true)
+                        {
+                            flipForwardsButton.gameObject.SetActive(false);
+                        }
+                        else
+                        {
+                            flipForwardsButton.gameObject.SetActive(true);
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void FlipBackwards()
+    {
+        //TODO: Do some flip-animation
+
+        if (enabledDiaryNotes != null)
+        {
+            for (int i = 0; i < enabledDiaryNotes.Length; i++)
+            {
+                if (enabledDiaryNotes[i] == null) { continue; }
+                if (enabledDiaryNotes[i].gameObject.activeSelf)
+                {
+                    enabledDiaryNotes[i].gameObject.SetActive(false);
+                    if (noteEnabled[i]) { flipForwardsButton.gameObject.SetActive(true); }
+
+                    if ((i - 1) >= 0)
+                    {
+                        enabledDiaryNotes[i - 1].gameObject.SetActive(true);
+
+                        if ((i - 2) < 0)
+                        {
+                            flipBackwardsButton.gameObject.SetActive(false);
+                        }
+                        else
+                        {
+                            flipBackwardsButton.gameObject.SetActive(true);
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void SetNextNoteActive()
+    {
+        for (int i = 0; i < noteEnabled.Length; i++)
+        {
+            if (!noteEnabled[i])
+            {
+                noteEnabled[i] = true;
+                enabledDiaryNotes[i] = diaryNotes[i];
+
                 break;
             }
         }
